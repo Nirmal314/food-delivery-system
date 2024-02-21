350282466bd04f9e96b1b4fcf1f5876a8b825509
Nirmal Ambasana <sit441@sit.local>
2024-02-21 18:48:46 +0530

    added credential auth with prisma, bug remaining in google auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,28 @@
 import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcryptjs";
 
 export default {
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+        console.log(validatedFields);
+        // @ts-ignore
+        const { email, password } = validatedFields.data;
+        const user = await getUserByEmail(email);
+        if (!user || !user.password) return null;
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (passwordsMatch) return user;
+        return null;
+      },
     }),
   ],
 } satisfies NextAuthConfig;
