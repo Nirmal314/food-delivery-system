145466559642e49f4ba9a38c1ebaeb4660faed3b
deepgajjar0265 <161486473+deepgajjar0265@users.noreply.github.com>
2024-03-16 12:34:05 +0530

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -26,7 +26,7 @@ export default {
         const passwordsMatch = await bcrypt.compare(password, user.password);
         if (passwordsMatch) return user;
         
-        return null;
+        return undefined;
       },
     }),
   ],
