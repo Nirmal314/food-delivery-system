a105d8e5886d99a5bf97661517507ba7191d2886
Nirmal Ambasana <sit441@sit.local>
2024-03-19 16:53:17 +0530

    menuitem image rendering done with loading, item(s) deletion done

---
+++ actions/admin/deletemenuitem.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export const deleteMenuItems = async (values: string[]) => {
+  try {
+    await db.menuItem.deleteMany({
+      where: {
+        id: {
+          in: values,
+        },
+      },
+    });
+
+    revalidatePath("/menu");
+  } catch (e) {
+    console.log(e);
+    return { error: "Something went wrong." };
+  }
+
+  return { success: "Food items deleted!" };
+};
---
+++ components/LoadingSkeletons/MenuItemLoading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuItemLoading = () => {
+  return <div className="animate-pulse rounded-lg w-72 h-48 bg-gray-200"></div>;
+};
+
+export default MenuItemLoading;
