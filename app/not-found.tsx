3a0b30c769bec746cf1c7db5555f74dee859ea7e
Nirmal Ambasana <nambasana033@gmail.com>
2025-09-02 16:30:08 +0530

    SS 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,11 @@
-"use client";
-
 import Image from "next/image";
 import React from "react";
 import error404 from "@/public/404.webp";
 import { ArrowLeftIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const NotFound = () => {
-  const router = useRouter();
   return (
     <div className="w-full h-[85vh] flex flex-col items-center justify-center">
       <Image alt="404" src={error404} height={404} width={404}></Image>
@@ -19,13 +16,14 @@ const NotFound = () => {
         <p className="md:text-lg lg:text-xl text-gray-600 mt-8">
           Sorry, the page you are looking for could not be found.
         </p>
-        <Button
-          onClick={() => router.push("/")}
-          className="flex items-center space-x-2 px-4 py-2 mt-12 rounded transition duration-150"
-        >
-          <ArrowLeftIcon />
-          <span>Return Home</span>
-        </Button>
+        <Link href="/">
+          <Button
+            className="flex items-center space-x-2 px-4 py-2 mt-12 rounded transition duration-150"
+          >
+            <ArrowLeftIcon />
+            <span>Return Home</span>
+          </Button>
+        </Link>
       </div>
     </div>
   );
